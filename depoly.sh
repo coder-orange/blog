1aa4bd9fdf3db64908029fa9ff80ab5660798778
coder-cyc <c2271373724@163.com>
2021-07-20 16:20:47 +0800

    123123

--- depoly.sh
+++ depoly.sh
@@ -16,7 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:coder-orange/blog.git main:gh-pages
+git push -f https://github.com/coder-orange/blog.git master:gh-pages
 
 cd -
