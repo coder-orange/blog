3dbf6cacfa1fd91f1e2b5ac568506a68a300ce85
coder-cyc <c2271373724@163.com>
2021-07-23 18:20:15 +0800

    ok

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="container">
+  <div class="container-full">
     <BackGround>
       <SideBar></SideBar>
-      <div class="main">
-        <slot></slot>
+      <div class="container">
+        <div class="main">
+          <slot></slot>
+        </div>
       </div>
     </BackGround>
   </div>
@@ -22,7 +24,7 @@ export default {
 </script>
 
 <style lang="less">
-.container {
+.container-full {
   position: fixed;
   left: 0;
   top: 0;
@@ -30,11 +32,21 @@ export default {
   height: 100vh;
   box-sizing: border-box;
 }
-.main {
+.container {
   position: absolute;
   left: 0;
   top: 40px;
   width: 100%;
   height: calc(100% - 40px);
+  .main {
+    opacity: 0.5;
+    box-sizing: border-box;
+    width: 80vw;
+    height: calc(100% - 80px);
+    margin: 40px 10vw;
+    background: #fff;
+    border-radius: 30px;
+    box-shadow: 0px 0px 15px 3px rgba(255, 255, 255, 0.3);
+  }
 }
 </style>
